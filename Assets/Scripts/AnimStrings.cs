836851be34e513fa5973a6b783a88e46bdf62eaa
Adrianites <125776060+Adrianites@users.noreply.github.com>
2024-11-26 15:57:44 +0000

    added stuffs

--- Assets/Scripts/AnimStrings.cs
+++ Assets/Scripts/AnimStrings.cs
@@ -22,7 +22,7 @@ internal class AnimStrings
     internal static string isAlive = "IsAlive";
     internal static string HitTrigger = "Hit";
 
-    public static string lockVelocity = "LockVelocity";
+    internal static string lockVelocity = "LockVelocity";
     internal static string EnterPortal = "EnterPortal";
     internal static string ExitPortal = "ExitPortal";
 }
